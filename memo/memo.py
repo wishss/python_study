2d3b5809e8295af2654b9c6284ba185705d5d829
wishss <wishkang21@gmail.com>
2024-11-21 16:38:04 +0900

    파일명 커맨드창에서 설정할 수 있도록 변경 (DVT-73)

--- memo/memo.py
+++ memo/memo.py
@@ -2,15 +2,16 @@
 import sys
 
 option = sys.argv[1]
+filename = sys.argv[2]
 
 if option == '-a':
-    memo = sys.argv[2]
-    f = open('memo.txt', 'a')
+    memo = sys.argv[3]
+    f = open(filename, 'a')
     f.write(memo)
     f.write('\n')
     f.close()
 elif option == '-v':
-    f= open('memo.txt')
+    f= open(filename)
     memo = f.read()
     f.close()
     print(memo)
